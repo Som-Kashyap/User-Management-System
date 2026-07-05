68fce8beb5f6ec19b4f2ef5059fa42f7b0f2c8b9
Som-Kashyap <somkashyap0123@gmail.com>
2026-07-05 15:16:31 +0530

    add, addUser, viewUsers, exitProgram functions

--- User-Management.cpp
+++ User-Management.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+enum class state {
+	Menu,
+	AddUser,
+	ViewUsers,
+	Exit
+};
+
 class user {
 
 private:
@@ -25,27 +32,85 @@ public:
 	
 };
 
-int main() {
+void menu(int &choice) {
 
-	user u1;
+	cout << "1. Add User" << endl;
+	cout << "2. View Users" << endl;
+	cout << "3. Exit" << endl;
+	cout << "Enter your choice: ";
+	cin >> choice;
+
+}
+
+void addUser(connection& conn)
+{
+	string name;
+	int id;
+	string password;
+	string email;
+
+	cout << "Enter your Name... ";
+	cin >> name;
+	cout << "Got name\n";
+
+	cout << "Enter your ID... ";
+	cin >> id;
+	cout << "Got id\n";
+
+	cout << "Create Password... ";
+	cin >> password;
+	cout << "Got password\n";
+
+	cout << "Enter your Email... ";
+	cin >> email;
+	cout << "Got email\n";
+
+	conn.execute("INSERT INTO users (name, user_id, password, email) VALUES ('" + name + "', " + std::to_string(id) + ", '" + password + "', '" + email + "')");
+}
+
+void viewUsers(connection& conn) {
+
+	try {
+		conn.getData("SELECT * FROM users");
+	}
+	catch (sql::SQLException& e)
+	{
+		std::cout << "SQL Error: " << e.what() << '\n';
+		std::cout << "Error Code: " << e.getErrorCode() << '\n';
+		std::cout << "SQLState: " << e.getSQLState() << '\n';
+	}
+}
+
+void exitProgram() {
+	cout << "Program terminated..." << endl;
+}
+
+void logic(int &choice , connection& conn) {
+
+	switch (choice) {
+
+	case 1:addUser(conn);
+		   break;
+
+	case 2: viewUsers(conn);
+		   break;
+
+	case 3: exitProgram();
+		break;
+
+	}
+}
+
+int main()
+{
+
 	connection conn("tcp://127.0.0.1:3306", "root", "password");
 
-	u1.setDetails("JohnDoe", 1, "password123", "joh123@gmail.com");
+	int choice;
 
-	//conn.execute("INSERT INTO users (name, user_id, password, email) VALUES ('JohnDoe', 1, 'password123', 'joh123@gmail.com')");
-	//conn.getData("SELECT * FROM users");
+	menu(choice);
 
-	user u2;
-	u2.setDetails("JaneDoe", 2, "password456", "jane456@gmail.com");
-
-	//conn.execute("INSERT INTO users (name, user_id, password, email) VALUES ('JaneDoe', 2, 'password456', 'jane456@gmail.com')");
-	//conn.getData("SELECT * FROM users");
-
-	user u3;
-	//u3.setDetails("AliceSmith", 3, "password789", "alice123@gmail.com");
-	//conn.execute("INSERT INTO users (name, user_id, password, email) VALUES ('AliceSmith', 3, 'password789', 'alice123@gmail.com')");
-
-	conn.getData("SELECT * FROM users");
+	logic(choice, conn);
 
 	return 0;
 }
